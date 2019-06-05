2347a03b97fd1c8d5319770a9627890c99846d84
Thiago Maciel <thiagomrvieira@gmail.com>
2019-06-05 09:39:36 -0300

    Redirecionando do login para outra página

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/produtos';
 
     /**
      * Create a new controller instance.
